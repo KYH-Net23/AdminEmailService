03009364e408af30e79011123f9c3f8a12262cee
kahol <kahola@gmail.com>
2024-11-12 18:40:54 +0100

    email service template

---
+++ EmailProvider/Services/EmailService.cs
@@ -0,0 +1,13 @@
+﻿using EmailProvider.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailProvider.Services;
+
+public class EmailService(DataContext dbContext)
+{
+    public async Task<List<Email>> GetEmailsAsync()
+    {
+        var emails = await dbContext.Emails.ToListAsync();
+        return emails;
+    }
+}
