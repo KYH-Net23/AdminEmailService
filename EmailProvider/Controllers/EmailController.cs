c57e1b692c2057763663bf4a37efa8d6bddb37f6
kahol <kahola@gmail.com>
2024-11-12 18:41:09 +0100

    template controller

---
+++ EmailProvider/Controllers/EmailController.cs
@@ -0,0 +1,17 @@
+﻿using EmailProvider.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailProvider.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmailController(EmailService emailService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var result = await emailService.GetEmailsAsync();
+
+        return Ok(result);
+    }
+}
